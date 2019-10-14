bf6f2aad3451e53895920ee900b3a204818ad81c
Sopoat <nineeebbomber@gmail.com>
2019-10-14 22:23:44 +0700

    Add show contents

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 window.onload = start;
 count = 0;
+document.querySelector('.warp-parallax').addEventListener('scroll', checkCenterScreen);
 function start() {
     setTimeout(() => {
         raining = setInterval(() => {
@@ -18,12 +19,62 @@ function start() {
             }
         }, 150);
     }, 4000);
-    setTimeout(() => {
-        let flower = document.getElementsByClassName('flower');
-        for (let i = 0; i < flower.length; i++) {
-            flower[i].style.transform = 'scale(1)'
-        }
-    }, 4500);
+}
+function checkCenterScreen() {
+    let warp = document.querySelector('.warp-parallax');
+    let centerScreen = warp.getBoundingClientRect().height / 2;
+    let checkPointScreen = warp.scrollTop + centerScreen;
+    // for show1
+    let show1PosStart = document.querySelector('.title-picture').offsetTop;
+    let show1PosEnd = document.querySelector('.title-picture').offsetTop +
+        document.querySelector('.title-picture').getBoundingClientRect().height +
+        document.querySelector('#content1').getBoundingClientRect().height;
+    if (checkPointScreen > show1PosStart && checkPointScreen <= show1PosEnd) {
+        showContent1(true);
+    }
+    else {
+        showContent1(false);
+    }
+
+    //for show2
+    let show2PosStart = document.querySelector('.background-image1').offsetTop;
+    let show2PosEnd = show2PosStart +
+        document.querySelector('.background-image1').getBoundingClientRect().height;
+    if (checkPointScreen > show2PosStart && checkPointScreen <= show2PosEnd) {
+        showContent2(true);
+    }
+    else {
+        showContent2(false);
+    }
+
+}
+
+function showContent1(x) {
+    let flower = document.querySelector('.flower');
+    if (x) {
+        flower.style.transform = 'scale(1)';
+        // 
+        content1.style.opacity = '1';
+        content1.style.transform = 'translateY(0px)'
+    }
+    else {
+        flower.style.transform = 'scale(0)';
+        // 
+        content1.style.opacity = '0';
+        content1.style.transform = 'translateY(100px)'
+    }
+}
+function showContent2(x) {
+    if (x) {
+        // 
+        content2.style.opacity = '1';
+        content2.style.transform = 'translateY(0px)';
+    }
+    else {
+        // 
+        content2.style.opacity = '0';
+        content2.style.transform = 'translateY(100px)';
+    }
 }
 
 function clearRianDrop() {
