07d6234e95088aa34dc91884d7cc0b3c0f03f78a
Sopoat <nineeebbomber@gmail.com>
2019-10-21 03:14:18 +0700

    Add history page contents

--- js/history.js
+++ js/history.js
@@ -10,6 +10,7 @@ function slide(x) {
     if (nowContent < 0)
         nowContent = 0
 
+    let start = document.querySelector('.start');
     let button = document.getElementsByClassName('botton');
     let content = document.getElementsByClassName('content');
 
@@ -20,14 +21,22 @@ function slide(x) {
     setTimeout(() => {
 
         if (nowContent == 0) {
-            button[0].style.display = 'none';
-            button[1].style.display = 'none';
-        } else {
-            button[0].style.display = 'block';
-            button[1].style.display = 'block';
+            start.style.transform = 'scale(1)';
+            button[0].style.transform = 'scale(0)';
+            button[1].style.transform = 'scale(0)';
+        }
+        else {
+            start.style.transform = 'scale(0)';
+            button[0].style.transform = 'scale(1)';
+            button[1].style.transform = 'scale(1)';
+        }
+
+        if (nowContent == 4) {
+            button[1].style.transform = 'scale(0)';
         }
 
         let slider = document.querySelector('.slider');
         slider.style.transform = `translateX(${-100 * nowContent}vw)`
     }, 200);
+
 }
