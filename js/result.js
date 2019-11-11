e6a3d32f73d68dd10a1795adf190f286e9652bac
Wassapol Pungjap <pptvstudio@gmail.com>
2019-11-12 00:28:36 +0700

    decorate result.html :bouquet:

---
+++ js/result.js
@@ -0,0 +1,42 @@
+document.querySelector('.wraper').addEventListener('scroll', checkCenterScreen);
+
+function checkCenterScreen() {
+    let warp = document.querySelector('.wraper');
+    let centerScreen = warp.getBoundingClientRect().height / 2;
+    let checkPointScreen = warp.scrollTop + centerScreen;
+    // for show1
+    let show1PosStart = document.querySelector('.title-picture').offsetTop;
+    let show1PosEnd = document.querySelector('.title-picture').offsetTop +
+        document.querySelector('.title-picture').getBoundingClientRect().height +
+        document.querySelector('#content1').getBoundingClientRect().height;
+    if (checkPointScreen > show1PosStart && checkPointScreen <= show1PosEnd) {
+        showContent1(true);
+    }
+    else {
+        showContent1(false);
+    }
+
+    //for show2
+    let show2PosStart = document.querySelector('.background-image1').offsetTop;
+    let show2PosEnd = show2PosStart +
+        document.querySelector('.background-image1').getBoundingClientRect().height;
+    if (checkPointScreen > show2PosStart && checkPointScreen <= show2PosEnd) {
+        showContent2(true);
+    }
+    else {
+        showContent2(false);
+    }
+
+}
+
+function showContent1(x) {
+    let content = document.querySelector('.content-wrap');
+    if (x) {
+        content.style.opacity = '1';
+        content.style.transform = 'translateY(0px)'
+    }
+    else {
+        content.style.opacity = '0';
+        content.style.transform = 'translateY(100px)'
+    }
+}
