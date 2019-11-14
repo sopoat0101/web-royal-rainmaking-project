5b1158b1d273c302945070fad1efa49adc1a6de5
Wassapol Pungjap <pptvstudio@gmail.com>
2019-11-14 14:31:37 +0700

    update result page. :lipstick:

--- js/result.js
+++ js/result.js
@@ -15,10 +15,6 @@ function scroll(){
 
     let content1Start = bannerEnd;
     let content1End = document.querySelector('#content1').offsetTop + document.querySelector('#content1').getBoundingClientRect().height;
-    console.clear();
-    console.log(content1Start);
-    console.log(scroll);
-    console.log(content1End);
 
     if(scroll >= content1Start && scroll <= content1End){
         showMainContent1(true);
@@ -27,6 +23,29 @@ function scroll(){
         showMainContent1(false);
     }
 
+    let content2Start = content2.offsetTop;
+    let content2End = scrollable;
+    
+
+    if(scroll >= content2Start && scroll <= content2End){
+        showMainContent2(true);
+    }
+    else{
+        showMainContent2(false);
+    }
+
+    let flowerStart = document.querySelector('.result-img-bg');
+    console.clear();
+    console.log(scrollable - flowerStart.getBoundingClientRect().height);
+    console.log(scroll);
+    console.log(scrollable);
+
+    if(scroll >= flowerStart.offsetTop && scroll <= scrollable){
+        flowerStart.style.transform = 'translateY(0vw) scale(1)';
+    }
+    else{
+        flowerStart.style.transform = 'translateY(20vw) scale(0)';
+    }
 }
 
 function showContent1(x){
@@ -64,7 +83,37 @@ function showMainContent1(x){
             content1_main_left[i].style.opacity = '0';
             content1_main_left[i].style.transform = 'translateX(-10vw)';
             content1_main_right[i].style.opacity = '0';
-            content1_main_right[i].style.transform = 'translateX(10vw)'; //
+            content1_main_right[i].style.transform = 'translateX(10vw)';
+        }
+    }
+}
+
+function showMainContent2(x){
+    let leftContent = document.querySelectorAll('.left');
+    let rightContent = document.querySelectorAll('.right');
+    let header = document.querySelector('.head-content');
+    if(x){
+        header.style.opacity = '1';
+        header.style.transform = 'translateY(0vw)';
+        for(i = 0; i< rightContent.length; i++){
+            rightContent[i].style.opacity = '1';
+            rightContent[i].style.transform = 'translateX(0vw)';
+        }
+        for(i = 0; i < leftContent.length; i++){
+            leftContent[i].style.opacity = '1';
+            leftContent[i].style.transform = 'translateX(0vw)';
+        }
+    }
+    else{
+        header.style.opacity = '0';
+        header.style.transform = 'translateY(-5vw)';
+        for(i = 0; i< rightContent.length; i++){
+            rightContent[i].style.opacity = '0';
+            rightContent[i].style.transform = 'translateX(10vw)';
+        }
+        for(i = 0; i < leftContent.length; i++){
+            leftContent[i].style.opacity = '0';
+            leftContent[i].style.transform = 'translateX(-10vw)';
         }
     }
 }
